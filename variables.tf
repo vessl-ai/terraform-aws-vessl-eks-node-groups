3f78c5644f1dff78a7fd13030e1056dca96777fd
Sean Lee <sean@vessl.ai>
2023-06-26 21:15:14 +0900

    First commit

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "cluster_name" {}
+variable "cluster_version" {}
+variable "cluster_endpoint" {}
+variable "cluster_certificate_authority_data" {}
+variable "tags" {}
+variable "security_group_ids" {}
+
+variable "manager_node_ami_id" {}
+variable "manager_node_instance_type" {}
+variable "manager_node_disk_size" {}
+variable "manager_node_subnet_ids" {}
+
+variable "self_managed_node_groups_data" {
+  default = {}
+}
+
